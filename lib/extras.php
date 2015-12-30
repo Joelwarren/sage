a28f2b385aceec8976846cb2f9a39925175e3444
Joel Warren <joel@wiild.com.au>
2015-12-30 11:54:23 +1100

    add copyright and credit to footer

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,49 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Get First Post Date Function
+ */
+function first_post_date( $format = "Y-m-d" ) {
+  // Setup get_posts arguments
+  $args = array(
+    'numberposts' => 1,
+    'post_status' => 'publish',
+    'order' => 'ASC'
+  );
+
+  // Get all posts in order of first to last
+  $get_all = get_posts( $args );
+
+  if( !empty($get_all) ) {
+    // Extract first post from array
+    $first_post = $get_all[0];
+
+    // Assign first post date to var
+    $first_post_date = $first_post->post_date;
+
+    // return date in required format
+    $output = date( $format, strtotime($first_post_date) );
+
+    return $output;
+  } else {
+    return date('Y');
+  }
+}
+
+/**
+ * Create copyright label from publish dates on site
+ */
+function copyright_year() {
+
+  $first_year = first_post_date('Y');
+  $this_year = date('Y');
+
+  if( $first_year != $this_year ) {
+    return 'Copyright &copy; ' . $first_year . ' - ' . $this_year . ' ' . get_bloginfo('name');
+  } else {
+    return 'Copyright &copy; ' . $this_year . ' ' . get_bloginfo('name');
+  }
+
+}
--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,17 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+
+  <div class="site-meta">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 copyright">
+          <?php echo Roots\Sage\Extras\copyright_year(); ?></span>
+        </div><!-- .col -->
+        <div class="col-md-6 credit">
+          
+        </div><!-- .col -->
+      </div><!-- .row -->
+    </div><!-- .container -->
+  </div><!-- .site-meta -->
 </footer>
