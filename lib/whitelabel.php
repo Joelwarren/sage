9f1b877dde92914440bd22c4653764d2e6c2bdf4
Joel <joel@wiild.com.au>
2018-04-19 21:45:38 +1000

    use custom logo for login

--- lib/whitelabel.php
+++ lib/whitelabel.php
@@ -5,12 +5,14 @@ add_action('login_head', 'theme_login_logo');
 * Custom login page logo
 */
 function theme_login_logo() {
-    $admin_logo = ( get_theme_mod('admin_login_logo') != '' ? get_theme_mod('admin_login_logo') : get_stylesheet_directory_uri() . '/dist/images/login-logo.png' );
+    $custom_logo_id = get_theme_mod( 'custom_logo' );
+    $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 
     echo '<style type="text/css">
+          body.login { background: #000; }
           .login h1 a {
-          background-image:url(' . $admin_logo . ') !important;
-          background-size: 320px 80px !important;
+          background-image:url(' . $image[0] . ') !important;
+          background-size: contain !important;
           width: 320px !important; height: 80px !important; }
           </style>';
 }
